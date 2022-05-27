762ec19277bbc14737e0541345ae3acb8f8d893a
ashish13445 <65503198+ashish13445@users.noreply.github.com>
2022-05-27 20:17:02 +0530

    new build

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'http://localhost:5000/';
+const ENDPOINT = 'https://instant-chat-application.herokuapp.com/';
 
 let socket;
 
